c203e95620e880278b93990b1c421abd98c879fb
tomi119000 <135408771+tomi119000@users.noreply.github.com>
2025-09-02 15:21:06 +0900

    9/2 授業終了時点

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;  //名前空間（ライブラリ of）
+﻿using System.Runtime.CompilerServices;
+using UnityEngine;  //名前空間（ライブラリ of）
 
 // Class difinition: MonoBehavior Classから継承 
 // public class class名 -> public にするとInspector上に表示される(=Component= Instanse)
@@ -6,6 +7,7 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rbody; //PlayerについているRigidbody2Dを扱うための変数（Rigidbody2D型）
     float axisH; //入力の方向を記憶するための変数
+    public float speed = 3.0f;  //publicでUnityのInspector/PlayerControllerに表示
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -19,6 +21,13 @@ public class PlayerController : MonoBehaviour
         //Velocityの元となる値の取得（右なら1.0f, 左なら-1.0f、何もなければ0）
         axisH = Input.GetAxisRaw("Horizontal");
         // Vector2(Vector型) ：一次的にメモリに値を確保して目的となる変数に参照してもらう
-        rbody.linearVelocity = new Vector2(axisH * 3.0f, rbody.linearVelocity.y);
+        //PCの性能などに依存し、そのフレームレートで更新される--> FixedUpdate()でフレームレート固定
+        //rbody.linearVelocity = new Vector2(axisH, 0);
     }
+    //1秒間に50回繰り返す(FixedUpdate)ように制御しながら行う繰り返しメソッド
+    private void FixedUpdate()
+    {
+        rbody.linearVelocity = new Vector2(axisH * speed, rbody.linearVelocity.y);
+    }
+
 }
