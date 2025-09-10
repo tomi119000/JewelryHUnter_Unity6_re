195bf6851bded9a064154d9cd6c3848197f7ba67
tomi119000 <135408771+tomi119000@users.noreply.github.com>
2025-09-10 11:32:31 +0900

    9/10 AM timetextの内容（TimeControllerのdisplaytime）をUIに表示する機能の実装

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.UI; //UI Objectを認識するため
+using TMPro; //TextMeshProを認識するため
 
 public class UIController : MonoBehaviour
 {
@@ -12,9 +13,13 @@ public class UIController : MonoBehaviour
     public Sprite gameClearSprite; //ゲームクリアの絵
     public Sprite gameOverSprite;  //ゲームオーバーの絵
 
+    TimeController timeCnt; //TimeController スクリプトを扱うための変数
+    public GameObject timeText; //timeText Objectを扱うための変数
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        timeCnt = GetComponent<TimeController>(); //TimeController script(Component)を取得
         buttonPanel.SetActive(false);  //存在を非表示
         Invoke("InactiveImage", 1.0f);  //時間差(ここでは1秒後)でメソッドを発動する
     }
@@ -40,8 +45,18 @@ public class UIController : MonoBehaviour
             //メイン画像オブジェクトのImageComponentが所持しているsprite（変数）にステージクリアの絵を挿入
             mainImage.GetComponent<Image>().sprite = gameOverSprite;
 
-            //リトライボタンObjectのButtonComponentが所持している変数interactableを無効化
+            //NextボタンObjectのButtonComponentが所持している変数interactableを無効化
             nextButton.GetComponent<Button>().interactable = false;
+        }
+        else if(GameManager.gameState == "playing")
+        {
+            //いったんdisplaytimeの数字を変数：timesに渡しておく
+            float times = timeCnt.displayTime;
+            timeText.GetComponent<TextMeshProUGUI>().text = Mathf.Ceil(times).ToString();
+            // toString methodでfloat型のtimesをstring型に変換してtextに挿入
+            //Mathf.Ceil methodで四捨五入
+
+
         }
     }
 
